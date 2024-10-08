52650c715d67ca63c968eacd436fb98e27fa3474
Daniel Nilsson <dannil+github@protonmail.com>
2024-10-08 20:12:28 +0200

    luci-app-squid: convert to JavaScript
    Assumption has been made that the config file is stored in /etc/squid
    since the principal package limits the sysconfdir to this directory. If
    that assumption is changed in the future we need to adjust the ACL.
    
    Signed-off-by: Daniel Nilsson <dannil+github@protonmail.com>

---
+++ applications/luci-app-squid/htdocs/luci-static/resources/view/squid.js
@@ -0,0 +1,56 @@
+'use strict';
+
+'require form';
+'require fs';
+'require uci';
+'require view';
+
+return view.extend({
+
+	load: function() {
+		return uci.load('squid')
+			.then(() => uci.get('squid', 'squid', 'config_file'));
+	},
+
+	render: function(config_file) {
+		var m, s, o;
+
+		m = new form.Map('squid', _('Squid'));
+
+		s = m.section(form.TypedSection, 'squid');
+		s.anonymous = true;
+		s.addremove = false;
+
+		s.tab('general', _('General Settings'));
+		s.tab('advanced', _('Advanced Settings'));
+
+		o = s.taboption('general', form.Value, 'http_port', _('Port'));
+		o.datatype = 'portrange';
+		o.placeholder = '0-65535';
+
+		o = s.taboption('general', form.Value, 'visible_hostname', _('Visible Hostname'));
+		o.datatype = 'string';
+		o.placeholder = 'OpenWrt';
+
+		o = s.taboption('general', form.Value, 'coredump_dir', _('Coredump files directory'));
+		o.datatype = 'string';
+		o.placeholder = '/tmp/squid';
+
+		o = s.taboption('advanced', form.TextValue, '_data');
+		o.wrap = false;
+		o.rows = 25;
+		o.rmempty = false;
+		o.cfgvalue = function(section_id) {
+			return fs.read(config_file);
+		};
+		o.write = function(section_id, value) {
+			if (value) {
+				var normalized = value.replaceAll('\r\n', '\n');
+				fs.write(config_file, normalized);
+			}
+		};
+
+		return m.render();
+	},
+
+});
